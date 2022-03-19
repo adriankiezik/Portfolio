df85dfe0f19549cf811fd96c8e636e2ad554e1c8
adriankiezik <kiezikadrian@gmail.com>
2022-03-19 21:37:14 +0100

    Create nav, header & skills section

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+import AOS from 'aos';
+
+AOS.init();
+
+const scroll = document.querySelector(".header__scroll");
+
+window.addEventListener("scroll", event => {
+    if(window.scrollY > 20) {
+        scroll.classList.add("header__fadeOut");
+    } else {
+        scroll.classList.remove("header__fadeOut");
+    }
+})
